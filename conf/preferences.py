89444e8c01113a889725f09df5f2197cbf3f60ca
Sebastian Alonso <alon.sebastian@gmail.com>
2011-12-11 00:44:37 -0300

    sms capability added, only for argentinian company movistar

---
+++ conf/preferences.py
@@ -0,0 +1,5 @@
+route = "google.com"
+email = "alon.sebastian@gmail.com"
+sms_alarm = False
+phone_company = "movistar"
+phone_number = "xxxxxxxx"
--- main.py
+++ main.py
@@ -1,13 +1,20 @@
-import time, os 
+import time, os
+import conf
+
 while True:
-    route = "google.com"
-    ping_route = "ping -c 1 -W 2 " + route + ' | grep "packet loss"'
+    ping_route = "ping -c 1 -W 2 " + conf.route + ' | grep "packet loss"'
     print ping_route
 
     result = os.popen(ping_route).read()
     if "100% packet loss" in result:
         print "server down"
         #server down, do procedure
+        Mailing.mail(conf.email, "Server is down", "The server you were monitoring is DOWN!")
+        print "email sent"
+        if conf.sms_alarm and conf.sms_company.lower() == "movistar":
+            Mailing.mail(conf.sms_number+"@sms.movistar.net.ar", "", "The server you were monitoring is DOWN!")
+            
+        time.sleep(600) #10 minutes
 
     time.sleep (10)
 
