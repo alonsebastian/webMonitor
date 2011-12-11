e962177ce5df7a5e66a1d3abf323dade7d5353e6
Sebastian Alonso <alon.sebastian@gmail.com>
2011-12-11 01:45:44 -0300

    Added last message functionality, the user implements a REST interface for it define it in the conf, and it's up and running

--- conf/preferences.py
+++ conf/preferences.py
@@ -6,3 +6,5 @@ phone_company = "movistar"
 #american companies available: Verizon, AT&T, T-Mobile, Sprint
 phone_number = "xxxxxxxxxx"
 #always use the 10 digit number
+message_url = "http://webpage.com/yourrestinterface"
+#user's rest interface that returns data on traffic, cpu usage, or whatever
--- main.py
+++ main.py
@@ -1,28 +1,39 @@
 import time, os
+from urllib2 import urlopen
 import conf
 
+content = ""
 while True:
     ping_route = "ping -c 1 -W 2 " + conf.route + ' | grep "packet loss"'
+    #ping -c 1 = count 1, -W 2 = timeout 2 seconds
     print ping_route
 
     result = os.popen(ping_route).read()
+    try:
+        content = urlopen(conf.message_url).read()
+    except urllib2.HTTPError:
+        print "wrong url or server down"    #most like 404
+
     if "100% packet loss" in result:
         print "server down"
+        if content != "":
+            message = "server down!" + content
+        else: message = "The server you were monitoring is DOWN!"
         #server down, do procedure
-        Mailing.mail(conf.email, "Server is down", "The server you were monitoring is DOWN!")
+
+        Mailing.mail(conf.email, "Server is down", message)
         print "email sent"
         if conf.sms_alarm and conf.sms_company.lower() == "movistar":
-            Mailing.mail(conf.sms_number+"@sms.movistar.net.ar", "", "The server you were monitoring is DOWN!")
+            Mailing.mail(conf.sms_number+"@sms.movistar.net.ar", "", message)
         if conf.sms_alarm and conf.sms_company.lower() == "verizon":
-            Mailing.mail(conf.sms_number+"@vtext.com", "", "The server you were monitoring is DOWN!")
+            Mailing.mail(conf.sms_number+"@vtext.com", "", message)
         if conf.sms_alarm and conf.sms_company.lower() == "at&t":
-            Mailing.mail(conf.sms_number+"@txt.att.net", "", "The server you were monitoring is DOWN!")
+            Mailing.mail(conf.sms_number+"@txt.att.net", "", message)
         if conf.sms_alarm and conf.sms_company.lower() == "sprint":
-            Mailing.mail(conf.sms_number+"@messaging.sprintpcs.com", "", "The server you were monitoring is DOWN!")
+            Mailing.mail(conf.sms_number+"@messaging.sprintpcs.com", "", message)
         if conf.sms_alarm and conf.sms_company.lower() == "t-mobile":
-            Mailing.mail(conf.sms_number+"@tmomail.net", "", "The server you were monitoring is DOWN!")
+            Mailing.mail(conf.sms_number+"@tmomail.net", "", message)
             
         time.sleep(600) #10 minutes
 
     time.sleep (10)
-
