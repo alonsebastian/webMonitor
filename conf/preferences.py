89f315806313fe7ba55f89ad0c4c4085adcf41eb
Sebastian Alonso <alon.sebastian@gmail.com>
2011-12-11 01:11:05 -0300

    added several american phone companies

--- conf/preferences.py
+++ conf/preferences.py
@@ -2,4 +2,7 @@ route = "google.com"
 email = "alon.sebastian@gmail.com"
 sms_alarm = False
 phone_company = "movistar"
-phone_number = "xxxxxxxx"
+#argentinian companies available: movistar
+#american companies available: Verizon, AT&T, T-Mobile, Sprint
+phone_number = "xxxxxxxxxx"
+#always use the 10 digit number
--- main.py
+++ main.py
@@ -13,6 +13,14 @@ while True:
         print "email sent"
         if conf.sms_alarm and conf.sms_company.lower() == "movistar":
             Mailing.mail(conf.sms_number+"@sms.movistar.net.ar", "", "The server you were monitoring is DOWN!")
+        if conf.sms_alarm and conf.sms_company.lower() == "verizon":
+            Mailing.mail(conf.sms_number+"@vtext.com", "", "The server you were monitoring is DOWN!")
+        if conf.sms_alarm and conf.sms_company.lower() == "at&t":
+            Mailing.mail(conf.sms_number+"@txt.att.net", "", "The server you were monitoring is DOWN!")
+        if conf.sms_alarm and conf.sms_company.lower() == "sprint":
+            Mailing.mail(conf.sms_number+"@messaging.sprintpcs.com", "", "The server you were monitoring is DOWN!")
+        if conf.sms_alarm and conf.sms_company.lower() == "t-mobile":
+            Mailing.mail(conf.sms_number+"@tmomail.net", "", "The server you were monitoring is DOWN!")
             
         time.sleep(600) #10 minutes
 
